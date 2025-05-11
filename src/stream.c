f49a9ffde4e093ebafc2378388a034092c99a611
Ichi <78321273+BeanZip@users.noreply.github.com>
2025-05-10 23:58:06 -0400

    fixed stream

--- src/stream.c
+++ src/stream.c
@@ -1,29 +1,30 @@
 #include "../include/Basic/stream.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void read(streams *stream, int readsize) {
-  printf("Allocating....\n");
-
   if (stream == NULL) {
     fprintf(stderr, "Invalid stream pointer\n");
     exit(-6);
   }
 
-  if (stream->writebuffer == NULL) {
-    fprintf(stderr, "Write buffer not allocated\n");
+  printf("File>> ");
+
+  stream->writebuffer = malloc(sizeof(char) * readsize);
+  scanf_s("%s", stream->writebuffer, readsize);
+  if (stream->writebuffer == NULL || strcmp(stream->writebuffer, "") == 0) {
+    fprintf_s(stderr, "Write buffer not allocated\n");
+    free(stream->writebuffer);
     exit(-7);
   }
-
-  printf("File>> ");
-  scanf_s("%s", stream->writebuffer, readsize);
 
   printf("\n Looking for %s....\n", stream->writebuffer);
 
   errno_t err = fopen_s(&stream->file, stream->writebuffer, "r");
   if (err != 0) {
-    fprintf(stderr, "Couldn't Open File %s\n", stream->writebuffer);
-    exit(-3);
+    fprintf_s(stderr, "Couldn't Open File %s\n", stream->writebuffer);
+    free(stream->writebuffer);
   }
 
   printf("Opened file %s\n", stream->writebuffer);
@@ -31,8 +32,8 @@ void read(streams *stream, int readsize) {
   stream->readbuffer = malloc(sizeof(char) * readsize);
   if (stream->readbuffer == NULL) {
     fprintf(stderr, "Failed to allocate read buffer\n");
+    free(stream->writebuffer);
     fclose(stream->file);
-    exit(-5);
   }
 
   while (fgets(stream->readbuffer, readsize, stream->file)) {
